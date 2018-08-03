d79e1ea15dec9e1ffb5ff2f2b52de420ce629a23
yanguanchuan <519213534@qq.com>
2018-08-03 10:12:38 +0800

    修改了module名称

---
+++ AppProject/src/main/java/com/wj/fragment/MyClass.java
@@ -0,0 +1,9 @@
+package com.wj.fragment;
+
+/**
+ * Created by lenovo on 2018/5/20.
+ */
+
+public class MyClass {
+    private String kk;
+}
---
+++ AppProject/src/main/java/com/wj/fragment/calllogs/AllFragment.java
@@ -0,0 +1,77 @@
+package com.wj.fragment.calllogs;/**
+ * Created by wangjiang on 2016/3/12.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wj.fragment.R;
+
+/**
+ * User: WangJiang(wangjiang7747@gmail.com)
+ * Date: 2016-03-12
+ * Time: 16:40
+ */
+public class AllFragment extends Fragment {
+    private static final String TAG = "AllFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_all, null);
+    }
+
+
+    @Override
+    public void onResume() {
+        Log.d(TAG, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.d(TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.d(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    public void onStart() {
+        Log.d(TAG, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.d(TAG, "onDetach");
+        super.onDetach();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.d(TAG, "onDestroyView");
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onActivityCreated");
+        super.onActivityCreated(savedInstanceState);
+    }
+}
